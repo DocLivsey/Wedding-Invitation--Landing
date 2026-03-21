8a74d1834aaa80fc0ae2e42af2a1fdda2ee0eec9
DocLivsey <doclivsey@users.noreply.github.com>
2026-03-21 14:04:29 +0300

    fix: errors with importing by project absolute paths

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,13 +1,3 @@
-import { renderHero } from "./hero.js";
-import { renderInvitation } from "./invitation.js";
-import { renderDateSection } from "./date-section.js";
-import { renderSchedule } from "./schedule.js";
-import { renderVenue } from "./venue.js";
-import { renderDressCode } from "./dress-code.js";
-import { renderWishes } from "./wishes.js";
-import { renderGuestForm } from "./guest-form.js";
-import { renderContacts } from "./contacts.js";
-
 export function renderLandingPage(config) {
   return `
     <main class="page">
